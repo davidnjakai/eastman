a29c7fcc7709c62d022bab0cd1d909bcb2a617f4
pankpinther <dvdseroney@gmail.com>
2016-05-19 06:15:53 -0400

    add formapplication views and urls + start on templates

--- formapplication/urls.py
+++ formapplication/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^hello/', views.hello, name = 'hello'),
+    url(r'^(?P<patient_id>[1-2]+)/$', views.detail, name='detail'),
+    url(r'^(?P<patient_id>[1-2]+)/surname/$', views.surname, name='surname'),
+    url(r'^(?P<patient_id>[1-2]+)/firstname/$', views.firstname, name='firstname'),
 ]
--- formapplication/views.py
+++ formapplication/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
-
+from django.template import loader
 # Create your views here.
 from django.http import HttpResponse
+from .models import Patient
 def index(request):
-   return HttpResponse("Hello, this is  the index page")
+   the_patients_list = Patient.objects.order_by('-id')[:2]
+   template = loader.get_template('formapplication/index.html')   
+   context = {
+	'the_patients_list':the_patients_list,
+   }
+   return HttpResponse(template.render(context, request))
 def hello(request):
    return HttpResponse("Hello, this is the hello page")
+def detail(request, patient_id):
+   return HttpResponse("This is patient number %s"%patient_id)
+def surname(request, patient_id):
+   response="The patient your examining is %s"
+   return HttpResponse(response%patient_id)
+def firstname(request, patient_id):
+   return HttpResponse("The firstname is %s"%patient_id)
